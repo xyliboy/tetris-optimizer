7071d5e86f4ffcc820fe40430f27c734b47f3136
gxylinas <woodengeorgio@gmail.com>
2026-07-14 14:02:45 +0300

    chore: remove optional package documentation files

--- internal/solver/doc.go
+++
@@ -1,2 +0,0 @@
-// Package solver finds the smallest square that fits a set of tetrominoes.
-package solver
--- internal/tetromino/doc.go
+++
@@ -1,2 +0,0 @@
-// Package tetromino contains the input model and validation rules.
-package tetromino
