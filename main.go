0ce91c5d28219659f2df3bcd33c5349b29a1049d
gxylinas <woodengeorgio@gmail.com>
2026-07-14 13:10:40 +0300

    feat: connect file input to solver CLI

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"tetris-optimizer/internal/solver"
+	"tetris-optimizer/internal/tetromino"
+)
 
 func main() {
-	fmt.Println("Tetris Optimizer")
+	if err := run(os.Args[1:]); err != nil {
+		fmt.Println("ERROR")
+	}
+}
+
+func run(args []string) error {
+	if len(args) != 1 {
+		return tetromino.ErrInvalidFormat
+	}
+
+	data, err := os.ReadFile(args[0])
+	if err != nil {
+		return err
+	}
+	pieces, err := tetromino.Parse(string(data))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(solver.Solve(pieces))
+	return nil
 }
